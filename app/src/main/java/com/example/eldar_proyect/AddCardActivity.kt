24880741bde465135911372ac2c3ef075224be0e
adrian de seta <adriandeseta1985@gmail.com>
2024-08-23 14:42:37 -0300

    mainFix

--- app/src/main/java/com/example/eldar_proyect/AddCardActivity.kt
+++ app/src/main/java/com/example/eldar_proyect/AddCardActivity.kt
@@ -3,9 +3,12 @@ package com.example.eldar_proyect
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.eldar_proyect.data.UserData
 import com.example.eldar_proyect.databinding.ActivityAddCardBinding
+import com.example.eldar_proyect.dto.UserInfo
 
 class AddCardActivity : AppCompatActivity() {
 
@@ -47,5 +50,19 @@ class AddCardActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
+
+        binding.btnAddNewCard.setOnClickListener{
+            if (binding.addCardUserName.text.isEmpty() || binding.addCardUserSurname.text.isEmpty() || binding.addCardUserNumber.text.isEmpty()){
+                Toast.makeText(this, "Debe completar todos los campos campos", Toast.LENGTH_SHORT).show()
+            } else{
+                val userName =binding.addCardUserName.text.toString()
+                val userSurname =binding.addCardUserSurname.text.toString()
+                val userCardNumber =binding.addCardUserNumber.text.toString()
+
+                val userInfo = UserInfo(userName, userSurname, userCardNumber)
+
+                println(userInfo)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/eldar_proyect/dto/UserInfo.kt
@@ -0,0 +1,10 @@
+package com.example.eldar_proyect.dto
+
+data class UserInfo(
+
+    val userName: String?,
+
+    val userSurname: String?,
+
+    val userCardNumber: String?,
+)
