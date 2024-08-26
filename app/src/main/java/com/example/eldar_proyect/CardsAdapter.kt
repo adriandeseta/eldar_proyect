dadbc880942ed9ec7b2ca418930ea0a328ca9a6f
adrian de seta <adriandeseta1985@gmail.com>
2024-08-26 02:04:14 -0300

    encryptado

--- app/src/main/java/com/example/eldar_proyect/CardsAdapter.kt
+++ app/src/main/java/com/example/eldar_proyect/CardsAdapter.kt
@@ -1,9 +1,10 @@
+package com.example.eldar_proyect
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.eldar_proyect.R
 import com.example.eldar_proyect.dto.UserInfo
 
 class CardsAdapter(private val cardList: MutableList<UserInfo>) : RecyclerView.Adapter<CardsAdapter.CardViewHolder>() {
@@ -12,6 +13,8 @@ class CardsAdapter(private val cardList: MutableList<UserInfo>) : RecyclerView.A
         val userCardNumber: TextView = itemView.findViewById(R.id.card_number)
         val userName: TextView = itemView.findViewById(R.id.card_name)
         val userSurname: TextView = itemView.findViewById(R.id.card_surname)
+        val userCardType: TextView = itemView.findViewById(R.id.card_type)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
@@ -24,6 +27,8 @@ class CardsAdapter(private val cardList: MutableList<UserInfo>) : RecyclerView.A
         holder.userCardNumber.text = userInfo.cardNumber
         holder.userName.text = userInfo.name
         holder.userSurname.text = userInfo.surname
+        holder.userCardType.text = userInfo.cardType
+
     }
 
     override fun getItemCount(): Int {
